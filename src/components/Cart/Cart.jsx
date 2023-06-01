084e4a32ccf1180e9ddc0f64926952b849869adf
shahriar.mehedi <shahriar.mehedi@bjitgroup.com>
2023-06-01 12:38:00 +0600

    cart update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,37 +6,49 @@ import { useContext } from "react";
 import { Context } from "../../utils/context";
 
 const Cart = ({ setShowCart }) => {
-  const { cartItems, cartSubtotal } = useContext(Context);
-  console.log(cartSubtotal);
-  return (
-    <div className="cart-panel">
-      <div className="opac-layer"></div>
-      <div className="cart-content">
-        <div className="cart-header">
-          <span className="heading">Shopping Cart</span>
-          <span className="close-btn" onClick={() => setShowCart(false)}>
-            <MdClose />
-            <span className="text">Close</span>
-          </span>
+    const { cartItems, cartSubTotal } = useContext(Context);
+    return (
+        <div className="cart-panel">
+            <div className="opac-layer"></div>
+            <div className="cart-content">
+                <div className="cart-header">
+                    <span className="heading">Shopping Cart</span>
+                    <span
+                        className="close-btn"
+                        onClick={() => setShowCart(false)}
+                    >
+                        <MdClose />
+                        <span className="text">Close</span>
+                    </span>
+                </div>
+                {cartItems.length <= 0 ? (
+                    <div className="empty-cart">
+                        <BsCartX />
+                        <span>No Products in thr cart.</span>
+                        <button className="return-cta">Return to Shop</button>
+                    </div>
+                ) : (
+                    <>
+                        {" "}
+                        <CartItem />
+                        <div className="cart-footer">
+                            <div className="subtotal">
+                                <span className="text">SUBTOTAL :</span>
+                                <span className="text total">
+                                    {cartSubTotal}&#2547;
+                                </span>
+                            </div>
+                            <div className="button">
+                                <button className="checkout-cta">
+                                    Checkout
+                                </button>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
-        {/* <div className="empty-cart">
-            <BsCartX/>
-            <span>No Products in thr cart.</span>
-            <button className='return-cta'>Return to Shop</button>
-        </div> */}
-        <CartItem />
-        <div className="cart-footer">
-          <div className="subtotal">
-            <span className="text">SUBTOTAL :</span>
-            <span className="text total">{cartSubtotal}&#2547;</span>
-          </div>
-          <div className="button">
-            <button className="checkout-cta">Checkout</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Cart;
